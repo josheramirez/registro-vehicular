ca2099898e8e36c01491cc1df8590a5196e1e9c9
nicolas.693x@yahoo.com <nicolas.693x@yahoo.com>
2020-12-27 21:06:48 -0300

    se agrega seeder de usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(DepartamentoSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.cl',
+            'telefono' => '12345678',
+            'active' => 1,
+            'password' => Hash::make('123456')
+        ]);
+
+        DB::table('departamentos_usuarios')->insert([
+            'usuario_id' => 1,
+            'departamento_id' => 1,
+            'creador_id' => 1
+        ]);
+
+        DB::table('departamentos_usuarios')->insert([
+            'usuario_id' => 1,
+            'departamento_id' => 2,
+            'creador_id' => 1
+        ]);
+
+        DB::table('departamentos_usuarios')->insert([
+            'usuario_id' => 1,
+            'departamento_id' => 3,
+            'creador_id' => 1
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Funcionario',
+            'email' => 'funcionario@funcionario.cl',
+            'telefono' => '12345678',
+            'active' => 1,
+            'password' => Hash::make('123456')
+        ]);
+
+        DB::table('departamentos_usuarios')->insert([
+            'usuario_id' => 2,
+            'departamento_id' => 1,
+            'creador_id' => 1
+        ]);
+
+        DB::table('departamentos_usuarios')->insert([
+            'usuario_id' => 2,
+            'departamento_id' => 2,
+            'creador_id' => 1
+        ]);
+      
+    }
+}
